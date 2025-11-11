68fc9195c47fcac0695a0e875a17fff8a149af2b
planeslorena <planeslorena@gmail.com>
2025-11-11 20:29:17 -0300

    correccion conexion con backend

--- src/app/services/axios.ts
+++ src/app/services/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const createClient = () => {
   const client = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: process.env.NEXT_PUBLIC_API_URL
   });
   return client;
 }
