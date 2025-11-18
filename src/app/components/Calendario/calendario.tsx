4582648cb5b5e2f5f089d6c51c3f974b0eede2ac
planeslorena <planeslorena@gmail.com>
2025-11-18 19:26:57 -0300

    Cambio leyenda calendario

--- src/app/components/Calendario/calendario.tsx
+++ src/app/components/Calendario/calendario.tsx
@@ -70,8 +70,11 @@ export default function Calendario(props: any) {
             <div className='d-flex flex-column align-items-start justify-content-start w-100'>
                 <i className="bi bi-arrow-left" onClick={irAtras}></i>
             </div>
+            <p className="font-text h5 text-center m-1"> Proximamente se habilitarán los turnos! </p>
+                <p className="font-text h5 text-center m-1">Estate atento a nuestra redes sociales.</p> 
+                <p className="font-text h5 text-center m-1">Peluditos</p>
             <div className="d-flex flex-column align-items-center justify-content-center calendario-box">
-                <p className="font-text h5 text-center">Reserve aquí su turno para: <br />{turnoData?.mascota.nombre}</p>
+                {/*<p className="font-text h5 text-center">Reserve aquí su turno para: <br />{turnoData?.mascota.nombre}</p>*/}
                 <Calendar
                     className="mi-calendario"
                     locale="es-ES"
--- src/app/services/axios.ts
+++ src/app/services/axios.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 const createClient = () => {
   const client = axios.create({
     baseURL: 'https://peluditospoly.com.ar/api'
+    //'http://localhost:8080'
   });
   return client;
 }
