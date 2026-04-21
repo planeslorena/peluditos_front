4df054cff370339b79519dbcaa5455284e998792
planeslorena <planeslorena@gmail.com>
2026-04-21 15:50:47 -0300

    habilitacion calendario mayo

--- src/app/components/Calendario/calendario.tsx
+++ src/app/components/Calendario/calendario.tsx
@@ -15,8 +15,8 @@ export default function Calendario(props: any) {
     const { setMostrarMascotas, setMostrarCalendario, setMostrarHorarios }: { setMostrarMascotas: Function, setMostrarCalendario: Function, setMostrarHorarios: Function } = props;
     const [value, onChange] = useState<Value>();
     const { turnoData, setTurnoData } = useContext(TurnoContext);
-    const fechaMinima = moment('01/03/2026', 'DD/MM/YYYY').toDate() < moment(new Date()).toDate() ? moment(new Date()).toDate() : moment('01/03/2026', 'DD/MM/YYYY').toDate(); //moment(new Date()).add(1, 'days').toDate();
-    const fechaMaxima = moment('30/04/2026', 'DD/MM/YYYY').toDate(); //moment(new Date()).add(30, 'days').toDate();
+    const fechaMinima = moment().add(1, 'day').startOf('day').toDate();
+    const fechaMaxima = moment('31/05/2026', 'DD/MM/YYYY').toDate(); //moment(new Date()).add(30, 'days').toDate();
     const [diasNoDisponibles, setDiasNoDisponibles] = useState<string[]>([]);
     const [sinDia, setSinDia] = useState<string>();
 
